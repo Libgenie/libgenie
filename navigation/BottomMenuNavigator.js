c3f0f9d4697a05d427dcc7e61524a4d67c1c1f6b
saby22 <sabyasachi.tffs@gmail.com>
2021-04-22 02:04:11 +0530

    Added Bottom Navbar

---
+++ navigation/BottomMenuNavigator.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer} from 'react-navigation';
+import {MaterialIcons} from '@expo/vector-icons';
+
+import Dashboard from '../screens/Dashboard';
+import Search from '../screens/Search';
+import Logout from '../screens/Logout';
+
+const BottomMenuTabNavigator = createBottomTabNavigator({
+	Dashboard: {
+		screen: Dashboard,
+		navigationOptions: {
+			tabBarIcon: tabinfo => {
+				return <MaterialIcons name='dashboard' size={24} color='orange' />;
+			},
+		},
+	},
+	Search: {
+		screen: Search,
+		navigationOptions: {
+			tabBarIcon: tabinfo => {
+				return <MaterialIcons name='search' size={24} color='orange' />;
+			},
+		},
+	},
+	Logout: {
+		screen: Logout,
+		navigationOptions: {
+			tabBarIcon: tabinfo => {
+				return <MaterialIcons name='logout' size={24} color='orange' />;
+			},
+		},
+	},
+});
+
+export default createAppContainer(BottomMenuTabNavigator);
