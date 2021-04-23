b21f096c07556a9d1e234e9112bb058f8320c304
ishika1403 <ishikamukherjee14@gmail.com>
2021-04-23 19:41:44 +0530

    Implemented Login UI

---
+++ config/pallette.js
@@ -0,0 +1,10 @@
+const colors = {
+  lightGray: '#b3b3b3',
+  darkGreen: '#225a67',
+  seaGreen: '#33af85',
+  black: '#1e1e20',
+  screenbg: '#b8e2f2',
+  bg: '#eff0ed',
+};
+
+export default colors;
